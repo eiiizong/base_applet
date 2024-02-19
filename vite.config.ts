c4d4d46ba0437c4a3418df3ff1d1cde796fbd60e
eiiizong <eiiizong@163.com>
2024-02-19 16:24:54 +0800

    add vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-
+import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [
+    uni(),
+    eslintPlugin({
+      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+    }),
+  ],
 })
