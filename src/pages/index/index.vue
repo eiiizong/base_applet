ffc252d29bdc00110e1be4dc881400f241f484fa
eiiizong <eiiizong@163.com>
2024-02-19 17:21:06 +0800

    sass

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,25 +8,24 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref } from 'vue'
   const title = ref('Hello')
 </script>
 
-<style>
+<style lang="scss" scoped>
   .content {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-  }
-
-  .logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin-top: 200rpx;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 50rpx;
+    .logo {
+      height: 200rpx;
+      width: 200rpx;
+      margin-top: 200rpx;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 50rpx;
+    }
   }
 
   .text-area {
