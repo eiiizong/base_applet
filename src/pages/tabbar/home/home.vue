cf71e1dc311ba4ca09c5979f7c166cc37ad635c6
eiiizong <eiiizong@163.com>
2024-02-20 10:37:31 +0800

    pages

---
+++ src/pages/tabbar/home/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">home</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .home {
+    width: 100%;
+    min-height: 100%;
+    background: #efeffe;
+  }
+</style>
---
+++ src/pages/tabbar/my/my.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="my">my</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .my {
+    width: 100%;
+    min-height: 100%;
+    background: #efeffe;
+  }
+</style>
