80a966f0456ba47c59f8e2656922af65898a5ca9
eiiizong <eiiizong@163.com>
2024-02-20 11:58:21 +0800

    @unocss/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    '@unocss',
     './.eslintrc-auto-import.json',
   ],
   plugins: ['vue', '@typescript-eslint'],
