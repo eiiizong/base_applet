68f7f60fab64467646f4248ba9cccf31a90165a2
eiiizong <eiiizong@163.com>
2024-02-20 09:59:37 +0800

    AutoImport

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import eslintPlugin from 'vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { resolve } from 'path'
 
 const pathResolve = (dir: string): string => {
@@ -28,6 +30,29 @@ export default defineConfig(({ mode }) => {
       uni(),
       eslintPlugin({
         include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+      }),
+      // 加上下面的配置
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+        ],
+        imports: ['vue', 'uni-app', 'pinia'],
+        dts: 'auto-imports.d.ts',
+      }),
+      Components({
+        // dirs 指定组件所在位置，默认为 src/components
+        // 可以让我们使用自己定义组件的时候免去 import 的麻烦
+        dirs: ['src/components'],
+        // 配置需要将哪些后缀类型的文件进行自动按需引入，'vue'为默认值
+        // extensions: ['vue'],
+        // 解析组件，这里以 Element Plus 为例
+        // resolvers: [],
+        // 生成components.d.ts
+        dts: 'components.d.ts',
+        // 遍历子目录
+        deep: true,
       }),
     ],
   }
