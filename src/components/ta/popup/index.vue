314295d65e5bc5e00e75f9cb6a40ca3c4b708f53
eiiizong <eiiizong@163.com>
2024-03-01 17:36:35 +0800

    popup

--- src/components/ta/popup/index.vue
+++ src/components/ta/popup/index.vue
@@ -1,120 +1,178 @@
 <template>
-  <view v-if="inited" :class="rootClass" :style="rootStyle" @transitionend="onTransitionEnd">
+  <ta-overlay
+    v-if="overlay"
+    :show="show"
+    :z-index="zIndex"
+    :custom-style="overlayStyle"
+    :duration="duration"
+    @click="onClickOverlay"
+    :lock-scroll="lockScroll"
+    :root-portal="rootPortal"
+  />
+
+  <view
+    v-if="inited"
+    class="custom-class {{ classes }} {{ utils.bem('popup', [position, { round, safe: safeAreaInsetBottom, safeTop: safeAreaInsetTop, safeTabBar: safeAreaTabBar }]) }}"
+    style="{{ computed.popupStyle({ zIndex, currentDuration, display, customStyle }) }}"
+    @transitionend="onTransitionEnd"
+  >
     <slot />
+    <ta-icon
+      v-if="closeable"
+      :name="closeIcon"
+      class="close-icon-class van-popup__close-icon van-popup__close-icon--{{ closeIconPosition }}"
+      @click="emit('close')"
+    />
   </view>
 </template>
 
-<script lang="ts" setup>
-  import type { PropType } from 'vue'
-  import { computed, watch, nextTick } from 'vue'
-
-  import { bem } from '../utils'
-  import { GLOB_COMPONENT_CLASS_PREFIX } from '../constant'
-  import { requestAnimationFrame } from '../common/utils'
-  import { isObj } from '../common/validator'
-
-  interface Duration {
-    enter: number
-    leave: number
+<!-- 添加之后 组件之间可以样式穿透 目前未找到setup语法如何编写-->
+<!-- <script lang="ts">
+  export default {
+    options: { styleIsolation: 'shared' },
   }
-  type Status = 'leave' | 'enter' | ''
+</script> -->
 
-  const emit = defineEmits(['before-enter', 'enter', 'before-leave', 'leave', 'after-leave', 'after-enter'])
+<script lang="ts" setup>
+  import TaOverlay from '../overlay/index.vue'
+  import TaIcon from '../icon/index.vue'
+
+  import type { PropType } from 'vue'
+  import { computed, ref } from 'vue'
+
+  import { GLOB_COMPONENT_CLASS_PREFIX } from '../constant'
+  /**
+   * 基础类名
+   */
+  const basicClass = `${GLOB_COMPONENT_CLASS_PREFIX}-popup`
+
+  const emit = defineEmits([
+    'click',
+    'touchmove',
+    'close',
+    'click-overlay',
+    'before-enter',
+    'enter',
+    'after-enter',
+    'before-leave',
+    'leave',
+    'after-leave',
+  ])
   const props = defineProps({
     /**
-     * 动画类型
-     *
-     * fade 淡入
-     *
-     * fade-up 上滑淡入
-     *
-     * fade-down 下滑淡入
-     *
-     * fade-left 左滑淡入
-     *
-     * fade-right 右滑淡入
-     *
-     * slide-up 上滑进入
-     *
-     * slide-down 下滑进入
-     *
-     * slide-left 左滑进入
-     *
-     * slide-right 右滑进入
-     */
-    name: {
-      type: String as PropType<
-        | 'fade'
-        | 'fade-up'
-        | 'fade-down'
-        | 'fade-left'
-        | 'fade-right'
-        | 'slide-up'
-        | 'slide-down'
-        | 'slide-left'
-        | 'slide-right'
-        | ''
-      >,
-      default: () => 'fade',
-    },
-    /**
-     * 	是否展示组件
+     * 是否显示弹出层
      */
     show: {
       type: Boolean,
+      default: () => false,
+    },
+    /**
+     * z-index 层级
+     */
+    zIndex: {
+      type: Number,
+      default: () => 100,
+    },
+    /**
+     * 是否显示遮罩层
+     */
+    overlay: {
+      type: Boolean,
       default: () => true,
+    },
+    /**
+     * 弹出位置，可选值为 top bottom right left center
+     */
+    position: {
+      type: String as PropType<'center' | 'top' | 'right' | 'left' | 'top'>,
+      default: () => 'center',
     },
     /**
      * 动画时长，单位为毫秒
      */
     duration: {
-      type: [Number, Object] as PropType<Number | Duration>,
+      type: Number,
       default: () => 300,
     },
     /**
-     * 定义进入过渡的开始状态。在元素被插入之前生效，在元素被插入之后的下一帧移除。
+     * 是否显示圆角
      */
-    enterClass: {
+    round: {
+      type: Boolean,
+      default: () => false,
+    },
+    /**
+     * 是否锁定背景滚动，锁定时蒙层里的内容也将无法滚动
+     */
+    lockScroll: {
+      type: Boolean,
+      default: () => true,
+    },
+    /**
+     * 是否在点击遮罩层后关闭
+     */
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: () => true,
+    },
+    /**
+     * 是否显示关闭图标
+     */
+    closeable: {
+      type: Boolean,
+      default: () => false,
+    },
+    /**
+     * 关闭图标名称或图片链接
+     */
+    closeIcon: {
+      type: String,
+      default: () => 'cross',
+    },
+    /**
+     * 关闭图标位置，可选值为 top-left bottom-left bottom-right top-right
+     */
+    closeIconPosition: {
+      type: String as PropType<'top-left' | 'bottom-left' | 'bottom-right' | 'top-right'>,
+      default: () => 'top-right',
+    },
+    /**
+     * 是否为 iPhoneX 留出底部安全距离
+     */
+    safeAreaInsetBottom: {
+      type: Boolean,
+      default: () => true,
+    },
+    /**
+     * 是否留出顶部安全距离（状态栏高度）
+     */
+    safeAreaInsetTop: {
+      type: Boolean,
+      default: () => false,
+    },
+    /**
+     * 是否留出底部 tabbar 安全距离（在使用 tabbar 组件 & 小程序自定义 tabbar 时，popup 组件层级无法盖住 tabbar）
+     */
+    safeAreaTabBar: {
+      type: Boolean,
+      default: () => false,
+    },
+    /**
+     * 是否从页面中脱离出来，用于解决各种 fixed 失效问题，微信基础库 >= 2.25.2
+     */
+    rootPortal: {
+      type: Boolean,
+      default: () => false,
+    },
+    /**
+     * 	自定义遮罩层样式
+     */
+    overlayStyle: {
       type: String,
       default: () => '',
     },
     /**
-     * 定义进入过渡生效时的状态。在整个进入过渡的阶段中应用，在元素被插入之前生效，在过渡/动画完成之后移除。这个类可以被用来定义进入过渡的过程时间，延迟和曲线函数。
-     */
-    enterActiveClass: {
-      type: String,
-      default: () => '',
-    },
-    /**
-     * 定义进入过渡的结束状态。在元素被插入之后下一帧生效 (与此同时 enter-class 被移除)，在过渡/动画完成之后移除。
-     */
-    enterToClass: {
-      type: String,
-      default: () => '',
-    },
-    /**
-     * 定义离开过渡的开始状态。在离开过渡被触发时立刻生效，下一帧被移除
-     */
-    leaveClass: {
-      type: String,
-      default: () => '',
-    },
-    /**
-     * 定义离开过渡生效时的状态。在整个离开过渡的阶段中应用，在离开过渡被触发时立刻生效，在过渡/动画完成之后移除。这个类可以被用来定义离开过渡的过程时间，延迟和曲线函数。
-     */
-    leaveActiveClass: {
-      type: String,
-      default: () => '',
-    },
-    /**
-     * 定义离开过渡的结束状态。在离开过渡被触发之后下一帧生效 (与此同时 leave-class 被删除)，在过渡/动画完成之后移除
-     */
-    leaveToClass: {
-      type: String,
-      default: () => '',
-    },
-    /**
-     * 自定义根节点样式
+     * 自定义弹出层样式
      */
     customStyle: {
       type: String,
@@ -129,29 +187,17 @@
     },
   })
 
-  const classes = ref(`enter-class enter-active-class enter-to-class leave-class leave-active-class leave-to-class `)
-  const status = ref<Status>('')
   const inited = ref(false)
-  const display = ref(false)
-  const transitionEnded = ref(true)
-  const currentDuration = ref(300)
-  const enterFinishedPromise = ref<Promise<boolean> | null>(null)
 
   /**
    * 动态设置根标签类名
    */
   const rootClass = computed(() => {
     const { customClass } = props
-    let str = ''
-
-    str = bem('transition', {})
+    let str = basicClass
 
     if (customClass) {
       str += ` ${customClass}`
-    }
-
-    if (classes.value) {
-      str += ` ${classes.value}`
     }
 
     return str
@@ -161,16 +207,11 @@
    * 动态设置根标签样式
    */
   const rootStyle = computed(() => {
-    const { customStyle } = props
+    const { customStyle, zIndex } = props
     let str = ''
 
-    if (currentDuration.value) {
-      str += `transition-duration: ${currentDuration.value}ms; `
-      str += `-webkit-transition-duration: ${currentDuration.value}ms; `
-    }
-
-    if (!display.value) {
-      str += `display: none; `
+    if (zIndex) {
+      str += `z-index: ${zIndex}; `
     }
 
     if (customStyle) {
@@ -180,217 +221,30 @@
     return str
   })
 
-  const getClassNames = (name: string) => {
-    const prefix = GLOB_COMPONENT_CLASS_PREFIX
-    const { enterClass, enterActiveClass, enterToClass, leaveActiveClass, leaveClass, leaveToClass } = props
-    return {
-      enter: `${prefix}-${name}-enter ${prefix}-${name}-enter-active ${enterClass} ${enterActiveClass} `,
-      'enter-to': `${prefix}-${name}-enter-to ${prefix}-${name}-enter-active ${enterToClass} ${enterActiveClass} `,
-      leave: `${prefix}-${name}-leave ${prefix}-${name}-leave-active ${leaveClass} ${leaveActiveClass} `,
-      'leave-to': `${prefix}-${name}-leave-to ${prefix}-${name}-leave-active ${leaveToClass} ${leaveActiveClass}  `,
+  const onClickOverlay = () => {
+    emit('click-overlay')
+    const { closeOnClickOverlay } = props
+    if (closeOnClickOverlay) {
+      emit('close')
     }
   }
 
-  const enter = () => {
-    if (enterFinishedPromise.value) return
-
-    enterFinishedPromise.value = new Promise((resolve) => {
-      const { duration, name } = props
-      const classNames = getClassNames(name)
-      const _currentDuration = isObj(duration) ? duration.enter : duration
-
-      if (status.value === 'enter') {
-        return
-      }
-
-      status.value = 'enter'
-      emit('before-enter')
-
-      requestAnimationFrame(() => {
-        if (status.value !== 'enter') {
-          return
-        }
-
-        emit('enter')
-
-        inited.value = true
-        display.value = true
-        classes.value = classNames.enter
-        currentDuration.value = _currentDuration as number
-
-        requestAnimationFrame(() => {
-          if (status.value !== 'enter') {
-            return
-          }
-
-          transitionEnded.value = false
-          classes.value = classNames['enter-to']
-
-          resolve(true)
-        })
-      })
-    })
-  }
-
-  const leave = () => {
-    if (!enterFinishedPromise.value) return
-
-    enterFinishedPromise.value.then(() => {
-      if (!display.value) {
-        return
-      }
-
-      const { duration, name } = props
-      const classNames = getClassNames(name)
-      const _currentDuration = isObj(duration) ? duration.leave : duration
-
-      status.value = 'leave'
-      emit('before-leave')
-
-      requestAnimationFrame(() => {
-        if (status.value !== 'leave') {
-          return
-        }
-
-        emit('leave')
-
-        classes.value = classNames.leave
-        currentDuration.value = _currentDuration as number
-
-        requestAnimationFrame(() => {
-          if (status.value !== 'leave') {
-            return
-          }
-
-          transitionEnded.value = false
-          setTimeout(() => {
-            onTransitionEnd()
-            enterFinishedPromise.value = null
-          }, _currentDuration as number)
-          classes.value = classNames['leave-to']
-        })
-      })
-    })
-  }
-
-  const onTransitionEnd = () => {
-    if (transitionEnded.value) {
-      return
-    }
-
-    transitionEnded.value = true
-    if (status.value) {
-      emit(`after-${status.value}`)
-    }
-
-    const { show } = props
-    if (!show && display.value) {
-      display.value = false
+  const onTouchmove = () => {
+    const { lockScroll } = props
+    if (!lockScroll) {
+      emit('touchmove')
     }
   }
-
-  watch(
-    () => props.show,
-    (newVal, oldVal) => {
-      if (newVal === oldVal) {
-        return
-      }
-      if (newVal) {
-        nextTick(() => {
-          enter()
-        })
-      } else {
-        nextTick(() => {
-          leave()
-        })
-      }
-    }
-  )
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @use '../common/style/var.scss' as *;
-
-  .#{$namespace} {
-    &-transition {
-      transition-timing-function: ease;
-    }
-    &-fade {
-      &-enter-active,
-      &-leave-active {
-        transition-property: opacity;
-      }
-      &-enter,
-      &-leave-to {
-        opacity: 0;
-      }
-
-      &-down-enter-active,
-      &-down-leave-active,
-      &-left-enter-active,
-      &-left-leave-active,
-      &-right-enter-active,
-      &-right-leave-active,
-      &-up-enter-active,
-      &-up-leave-active {
-        transition-property: opacity, transform;
-      }
-
-      &-up-enter,
-      &-up-leave-to {
-        opacity: 0;
-        transform: translate3d(0, 100%, 0);
-      }
-
-      &-down-enter,
-      &-down-leave-to {
-        opacity: 0;
-        transform: translate3d(0, -100%, 0);
-      }
-
-      &-left-enter,
-      &-left-leave-to {
-        opacity: 0;
-        transform: translate3d(-100%, 0, 0);
-      }
-
-      &-right-enter,
-      &-right-leave-to {
-        opacity: 0;
-        transform: translate3d(100%, 0, 0);
-      }
-    }
-    &-slide {
-      &-down-enter-active,
-      &-down-leave-active,
-      &-left-enter-active,
-      &-left-leave-active,
-      &-right-enter-active,
-      &-right-leave-active,
-      &-up-enter-active,
-      &-up-leave-active {
-        transition-property: transform;
-      }
-
-      &-up-enter,
-      &-up-leave-to {
-        transform: translate3d(0, 100%, 0);
-      }
-
-      &-down-enter,
-      &-down-leave-to {
-        transform: translate3d(0, -100%, 0);
-      }
-
-      &-left-enter,
-      &-left-leave-to {
-        transform: translate3d(-100%, 0, 0);
-      }
-
-      &-right-enter,
-      &-right-leave-to {
-        transform: translate3d(100%, 0, 0);
-      }
-    }
+  ::v-deep .#{$namespace}-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--overlay-background-color, $overlay-background-color);
   }
 </style>
