8c959596a0d9b6f380ddef8d5dbc93836f38b19f
eiiizong <eiiizong@163.com>
2024-03-04 17:00:29 +0800

    type

---
+++ src/components/ta/types.ts
@@ -0,0 +1,8 @@
+import type { PropType } from 'vue'
+
+namespace Ta {
+  export type TransitionStaus = 'leave' | 'enter' | ''
+  export type TransitionPropsDuration = PropType<number>
+}
+
+export default Ta
