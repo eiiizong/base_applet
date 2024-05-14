f7927093f06395277db09f078ab608647fecbc3f
eiiizong <eiiizong@163.com>
2024-05-14 21:24:44 +0800

    login

---
+++ src/pages/packageCommon/login/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page login">
+    <div class="login-con">
+      <LoginHeader></LoginHeader>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  import LoginHeader from './part/LoginHeader.vue'
+  import { useUpdateNavigationBarTitle } from '@/hooks/useUpdateNavigationBarTitle'
+
+  onShow(() => {
+    useUpdateNavigationBarTitle()
+  })
+</script>
+
+<style lang="scss" scoped>
+  .login {
+    height: 100vh;
+    background-color: $color-primary;
+    overflow: auto;
+    padding: $spacing;
+    &-con {
+      width: 100%;
+      background-color: #fff;
+      border-radius: 4rpx;
+    }
+  }
+</style>
---
+++ src/pages/packageCommon/login/part/LoginHeader.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login-header">
+    <div class="avatar">
+      <image :src="avatarIcon" class="avatar" />
+    </div>
+
+    <!-- #ifdef MP-WEIXIN -->
+    <template>
+      <div class="title">
+        <span>请您进行人脸识别身份验证</span>
+      </div>
+      <div class="tip">
+        <span class="zh">信息将与当前微信绑定（用于身份验证）</span>
+      </div>
+    </template>
+    <!-- #endif -->
+
+    <!-- #ifndef MP-WEIXIN -->
+    <template>
+      <div class="title">
+        <span>请您登录</span>
+      </div>
+    </template>
+
+    <!-- #endif -->
+  </div>
+</template>
+<script setup lang="ts">
+  import avatarIcon from '../images/icon-avatar.png'
+</script>
+
+<style lang="scss" scoped>
+  .login-header {
+    width: 100%;
+    text-align: center;
+    padding: 26rpx 0;
+    color: $color-text;
+
+    .avatar {
+      width: 190rpx;
+      height: 174rpx;
+      margin: 0 auto;
+    }
+
+    .title {
+      width: 100%;
+      font-size: 30rpx;
+      font-weight: 600;
+      color: $color-text;
+      margin-top: 20rpx;
+      line-height: 1;
+    }
+
+    .tip {
+      width: 100%;
+      font-size: 26rpx;
+      color: $color-text-placeholder;
+      margin-top: 20rpx;
+      line-height: 1;
+
+      .zh {
+        font-weight: 400;
+      }
+
+      .zw {
+        font-size: 26rpx;
+      }
+    }
+  }
+</style>
