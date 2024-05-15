287e2a05ae98f6f0373e5a0f61d263b97f1aa1cb
eiiizong <eiiizong@163.com>
2024-05-15 14:21:03 +0800

    local

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,7 +1,9 @@
 import ja from './ja.json'
+import uniAppJa from './uni-app.ja.json'
 import zhHans from './zh-Hans.json'
+import uniAppZhHans from './uni-app.zh-Hans.json'
 
 export default {
-  ja: ja,
-  'zh-Hans': zhHans
+  ja: { ...uniAppJa, ...ja },
+  'zh-Hans': { ...uniAppZhHans, ...zhHans }
 }
