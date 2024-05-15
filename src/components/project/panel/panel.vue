f4822e07426768b245c022cecab00a28220d9c4f
eiiizong <eiiizong@163.com>
2024-05-15 21:27:16 +0800

    home

---
+++ src/components/project/panel/panel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="panel">
+    <div class="title">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+    <div class="con">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // import imageBgHeader from '../images/bg-header.png'
+  // import type { Store } from '@/stores/types'
+
+  // import { navigateTo } from '@/utils/uni'
+  // import { useI18n } from 'vue-i18n'
+  // import { useStoreUserSettings } from '@/stores/modules'
+  // import { useUpdateNavigationBarTitle } from '@/hooks/'
+
+  // const { locale } = useI18n()
+
+  // /**
+  //  * 切换语言
+  //  */
+  // const onClickSetLanguage = () => {
+  //   locale.value = locale.value === 'ja' ? 'zh-Hans' : 'ja'
+  //   useStoreUserSettings().updateStoreUserSettingsLanguage(locale.value as Store.UserSettings['language'])
+  //   useUpdateNavigationBarTitle()
+  // }
+</script>
+
+<style lang="scss" scoped>
+  .panel {
+    width: 690rpx;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/pages/tabbar/home/part/PopularServices.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-popular-services">23</div>
+</template>
+
+<script setup lang="ts">
+  // import imageBgHeader from '../images/bg-header.png'
+  // import type { Store } from '@/stores/types'
+
+  // import { navigateTo } from '@/utils/uni'
+  // import { useI18n } from 'vue-i18n'
+  // import { useStoreUserSettings } from '@/stores/modules'
+  // import { useUpdateNavigationBarTitle } from '@/hooks/'
+
+  // const { locale } = useI18n()
+
+  // /**
+  //  * 切换语言
+  //  */
+  // const onClickSetLanguage = () => {
+  //   locale.value = locale.value === 'ja' ? 'zh-Hans' : 'ja'
+  //   useStoreUserSettings().updateStoreUserSettingsLanguage(locale.value as Store.UserSettings['language'])
+  //   useUpdateNavigationBarTitle()
+  // }
+</script>
+
+<style lang="scss" scoped>
+  .home-popular-services {
+    width: 100%;
+  }
+</style>
