f0d40effdb5cfed520e1e42615ddb3999a1f202c
eiiizong <eiiizong@163.com>
2024-05-18 23:58:37 +0800

    优化

--- src/pages/packageQuery/project/part/QueryCriteria.vue
+++ src/pages/packageQuery/project/part/QueryCriteria.vue
@@ -5,9 +5,10 @@
         v-model="keyword"
         class="input"
         type="text"
-        id="name"
-        placeholder-style="color:#fff"
-        placeholder="请输入政策名称"
+        confirm-type="search"
+        placeholder-style="color:rgba(255, 255, 255, 0.8);"
+        placeholder="请输入关键字搜索"
+        @confirm="onSearch"
       />
     </div>
 
@@ -23,6 +24,10 @@
   const emit = defineEmits(['query'])
 
   const keyword = ref('')
+
+  const onSearch = () => {
+    console.log(keyword.value)
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -33,16 +38,18 @@
 
     .search-wrapper {
       padding: $spacing;
-      padding-top: 0;
       box-sizing: border-box;
       .input {
-        background-color: rgba(#fff, 0.3);
+        display: block;
+        width: 100%;
+        height: 72rpx;
         border-radius: 50px;
-        text-align: center;
-        padding: 20rpx;
+        background-color: rgba(#ffff, 0.6);
+        padding: 16rpx 24rpx;
         font-size: 28rpx;
         line-height: 40rpx;
-        height: 80rpx;
+        font-weight: 400;
+        text-align: left;
         color: #fff;
         box-sizing: border-box;
       }
