4f787c9b576f890a9bae8e0806746d27c3f1f216
eiiizong <eiiizong@163.com>
2024-05-18 17:13:47 +0800

    chaxun

---
+++ src/components/project/card-project-total/card-project-total.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="card-project">
+    <div class="name-wrapper">
+      <div class="name">计划生育家庭特别扶助制度扶助计划生育家庭特别扶助制度扶助</div>
+    </div>
+    <div class="cells">
+      <div class="cell">
+        <div class="value">555.99</div>
+        <div class="unit">万元</div>
+        <div class="key">涉及金额</div>
+      </div>
+      <div class="cell">
+        <div class="value">555</div>
+        <div class="unit">人</div>
+        <div class="key">覆盖户(人数)</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    /**
+     * 渲染内容
+     */
+    renderData: {
+      type: Object,
+      required: false
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  $color2: #f00;
+  .card-project {
+    width: 100%;
+    position: relative;
+    padding: 0 $spacing;
+    background-image: linear-gradient(to bottom, rgba($color-primary, 0.1) 0, #fff 80%);
+    background-color: #fff;
+    border-radius: 8rpx;
+    box-sizing: border-box;
+    .name-wrapper {
+      padding-top: $spacing;
+      .name {
+        font-size: 28rpx;
+        line-height: 36rpx;
+        max-height: 72rpx;
+        font-weight: 700;
+        color: $color-text;
+        @include textOverflow(2);
+      }
+    }
+    .cells {
+      display: flex;
+      padding: $spacing 0;
+
+      .cell {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        line-height: 1.4;
+        font-size: 24rpx;
+        margin-bottom: 12rpx;
+        border-right: 1px solid rgba($color-text, 0.1);
+        .key {
+          color: rgba($color-text, 0.6);
+          line-height: 1;
+          font-size: 26rpx;
+        }
+        .unit {
+          width: 100rpx;
+          border-radius: 40px;
+          text-align: center;
+          line-height: 40rpx;
+          font-size: 24rpx;
+          background-color: rgba($color-text, 0.1);
+          color: $color-text;
+          margin: 20rpx 0;
+        }
+        .value {
+          color: $color-text;
+          font-size: 32rpx;
+          font-weight: 700;
+          line-height: 1;
+        }
+        &:first-child {
+          .value {
+            color: $color2;
+          }
+          .unit {
+            background-color: rgba($color2, 0.1);
+            color: $color2;
+          }
+        }
+        &:last-child {
+          border-right: 0;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/packageQuery/publicity/part/PublicityMain.vue
+++ src/pages/packageQuery/publicity/part/PublicityMain.vue
@@ -1,8 +1,41 @@
 <template>
-  <div class="publicity-main"></div>
+  <div class="publicity-main">
+    <div class="title">按项目统计</div>
+    <div class="toolbar">
+      <picker class="picker"></picker>
+      <picker class="picker"></picker>
+      <picker class="picker"></picker>
+    </div>
+    <div class="main">
+      <scroll-view class="scroll-view tool" scroll-y>
+        <div class="item" v-for="item in 20" :key="item">{{ '农业农村局' + item }}</div>
+      </scroll-view>
+      <scroll-view class="scroll-view result" scroll-y>
+        <div class="total">
+          <div class="card">
+            <div class="value">66666</div>
+            <div class="unit">万元</div>
+            <div class="key">涉及金额</div>
+          </div>
+          <div class="card">
+            <div class="value">66666</div>
+            <div class="unit">人</div>
+            <div class="key">覆盖户(人数)</div>
+          </div>
+        </div>
+        <div class="items">
+          <div class="item" v-for="item in 20" :key="item">
+            <ComponentCardProjectTotal></ComponentCardProjectTotal>
+          </div>
+        </div>
+      </scroll-view>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ComponentCardProjectTotal from '@/components/project/card-project-total/card-project-total.vue'
+</script>
 
 <style lang="scss" scoped>
   .publicity-main {
@@ -10,5 +43,73 @@
     height: 100%;
     display: flex;
     flex-direction: column;
+
+    .main {
+      flex: 1;
+      overflow: hidden;
+      display: flex;
+      .tool {
+        width: 240rpx;
+        background-color: #fff;
+        padding-top: $spacing;
+        .item {
+          width: 100%;
+          font-size: 28rpx;
+          line-height: 1;
+          padding: 20rpx 10rpx;
+          text-align: center;
+          margin-bottom: 20rpx;
+          border-radius: 8rpx;
+          transition: all 0.3s;
+          &.checked {
+            background-color: rgba($color-primary, 0.2);
+          }
+        }
+      }
+    }
+    .scroll-view {
+      height: 100%;
+      overflow: hidden;
+
+      box-sizing: border-box;
+      &.result {
+        flex: 1;
+      }
+    }
+    .result {
+      padding-left: $spacing;
+      .total {
+        display: flex;
+        align-items: center;
+        font-size: 28rpx;
+        padding-right: $spacing;
+        .card {
+          width: 50%;
+          background-color: $color-primary;
+          padding: $spacing;
+          border-radius: 8rpx;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          color: #fff;
+          font-size: 24rpx;
+          line-height: 1;
+          .util {
+            width: 100rpx;
+            line-height: 40rpx;
+            text-align: center;
+            margin: 20rpx 0;
+          }
+        }
+      }
+      .items {
+        padding-right: $spacing;
+        padding-top: 20rpx;
+        .item {
+          margin-bottom: 20rpx;
+        }
+      }
+    }
   }
 </style>
