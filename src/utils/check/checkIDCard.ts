6f348182ce8d63df1d375481a223acb758637c7f
eiiizong <eiiizong@163.com>
2024-05-23 00:06:30 +0800

    登录

--- src/utils/check/checkIDCard.ts
+++ src/utils/check/checkIDCard.ts
@@ -3,33 +3,38 @@ interface IdCardCheckResult {
   errMsg: string // 错误提示信息
 }
 
-// 加权因子
-const Wi = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
-
-// 身份证验证位值 .10代表X
-const ValideCode = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]
-
 /**
  * 检验18位身份证的校验码是否正确。
  * 校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
- * @param valueArr 身份证号码组成的数组
+ * @param value 身份证号码组成的数组
  */
-const isTrueValidateIdCard = (valueArr: string[]) => {
+const isTrueValidateIdCard = (value: string[]) => {
+  // 加权因子
+  const Wi = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+
+  // 身份证验证位值 .10代表X
+  const ValideCode = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]
+
   let sum = 0 // 声明加权求和变量
 
-  if (valueArr[17].toLowerCase() === 'x') {
-    valueArr[17] = '10' // 将最后位为x的验证码替换为10方便后续操作
-  }
-
   for (let i = 0; i < 17; i++) {
-    sum += Wi[i] * Number(valueArr[i]) // 加权求和
+    sum += Wi[i] * Number(value[i]) // 加权求和
   }
   const valCodePosition = sum % 11 // 得到验证码所位置
 
-  if (valueArr[17] !== ValideCode[valCodePosition]) {
-    return false
+  const lastVal = value[17]
+  if (lastVal === 'X') {
+    if (lastVal !== ValideCode[valCodePosition]) {
+      return false
+    } else {
+      return true
+    }
   } else {
-    return true
+    if (Number(lastVal) !== ValideCode[valCodePosition]) {
+      return false
+    } else {
+      return true
+    }
   }
 }
 
